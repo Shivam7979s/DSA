aaf528642b3a6df2b339428c92d85491eff02d48
Shivam7979s <shivam.singh97989218@gmail.com>
2026-05-27 19:28:01 +0530

    Time: 5 ms (78.45%) | Memory: 45.9 MB (52.67%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -6,12 +6,8 @@ class Solution {
             int rem=x%10;
             reverse=reverse*10+rem;
         }
-        if(reverse==orignal){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return reverse==orignal;
+       
         
     }
 
