8d90df6e5fad79b1d05bdd5d24ea431cbd104c48
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-02 16:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (86.05%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<accounts.length;i++){
             int sum=0;
             for(int j=0;j<accounts[i].length;j++){
-                sum=sum+accounts[i][j];
+                sum+=accounts[i][j];
             }
             if(sum>ans){
                 ans=sum;
