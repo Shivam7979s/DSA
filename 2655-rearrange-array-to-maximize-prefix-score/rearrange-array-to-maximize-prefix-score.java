21b6916f3aaa0a2ee5d85d0707c6d4f4e835a8df
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-14 10:44:58 +0530

    Time: 33 ms (64.32%) | Memory: 78.6 MB (30.15%) - LeetSync

---
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxScore(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for(int i = 0, j = nums.length - 1; i < j; i++, j--){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        long sum = 0;
+        int max = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+
+            if(sum > 0){
+                max++;
+            }
+        }
+
+        return max;
+    }
+}
