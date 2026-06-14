911f296c231a80d7303f99588792487a53d5aa44
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-14 12:29:08 +0530

    Time: 135 ms (5.16%) | Memory: 47.5 MB (62.29%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -1,24 +1,32 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int ct=-1;
-        int ans=-1;
-        for(int i=0;i<nums.length;i++){
-            int count=-1;
 
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]%2==0 && nums[i]==nums[j] ){
+        int ct = 0;
+        int ans = -1;
+
+        for(int i = 0; i < nums.length; i++) {
+
+            if(nums[i] % 2 != 0) {
+                continue;
+            }
+
+            int count = 0;
+
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
                     count++;
                 }
-                
             }
-            if(ct<count){
-                ct=count;
-                ans=nums[i];
+
+            if(count > ct) {
+                ct = count;
+                ans = nums[i];
             }
-             else if(ct == count && nums[i] < ans){
+            else if(count == ct && nums[i] < ans) {
                 ans = nums[i];
             }
         }
-        return ans;  
+
+        return ans;
     }
 }
