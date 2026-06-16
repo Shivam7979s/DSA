5909ebecd997f39872a5104e614b05d0447038c9
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-16 19:07:06 +0530

    Time: 4 ms (80.16%) | Memory: 43.2 MB (85.01%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,13 +1,35 @@
 class Solution {
     public int heightChecker(int[] heights) {
-       int[] expected=heights.clone();
-       Arrays.sort(expected);
-       int count=0;
-       for(int i=0;i<heights.length;i++){
-        if(heights[i]!=expected[i]){
-            count++;
+        int[] exp=new int[heights.length];
+        int count=0;
+     
+
+        for(int i=0;i<heights.length;i++){
+           exp[i]=heights[i];
         }
-       }
-       return count; 
+
+        for(int i=0;i<exp.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(exp[j]<exp[j-1]){
+                    int temp=exp[j-1];
+                    exp[j-1]=exp[j];
+                    exp[j]=temp;
+
+                }
+                else{
+                    break;
+                }
+
+            }
+        }
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=exp[i]){
+                count++;
+
+            }
+        }
+       return count;
+
+        
     }
 }
