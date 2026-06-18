d432d99c421150f714e9b052b6383b26ddaa8fb4
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-18 22:49:57 +0530

    Time: 3 ms (64.41%) | Memory: 47.7 MB (50.64%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Arrays.sort(nums);
-        int[] arr=new int[2];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                arr[0]=nums[i];
-            
-            }
-        }
-        for(int i=0;i<nums.length;i++){
-            int ind=Math.abs(nums[i])-1;
-            if(nums[ind]>0){
-                nums[ind]*=-1;
-            }
-        }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                arr[1]=i+1;
+        int i = 0;
+
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if (nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                i++;
             }
         }
 
-        return arr;
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return new int[]{nums[i], i + 1};
+            }
+        }
+
+        return new int[]{-1, -1};
     }
 }
