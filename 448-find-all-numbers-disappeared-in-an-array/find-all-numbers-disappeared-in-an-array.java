11ff03e5dacc176d78430551f521689205ca57dc
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-18 21:30:38 +0530

    Time: 7 ms (48.20%) | Memory: 67 MB (83.85%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int n = nums.length;
-        
-        // First loop: mark numbers as visited by negating the value at their index
-        for (int i = 0; i < n; i++) {
-            int num = Math.abs(nums[i]);  // Get the value at current index
-            int index = num - 1; // Convert value to index (since numbers range from 1 to n)
-            nums[index] = -Math.abs(nums[index]); // Mark as visited by negating it
-        }
-        
-        // Second loop: check which indices stayed positive and add them to result
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) { // If it's still positive, it means we never visited this number
-                list.add(i + 1); // Add the missing number (index + 1)
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int index=Math.abs(nums[i])-1;
+            if(nums[index]<0){
+                continue;
+            }
+            else{
+            nums[index] *=-1;
             }
         }
-        
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                list.add(i+1);
+            }
+        }
         return list;
     }
 }
