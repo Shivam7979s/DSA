d47d561e610b987bc73bd879c7b8f43aa5cef7be
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-18 21:32:03 +0530

    Time: 6 ms (81.21%) | Memory: 66.9 MB (83.85%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,22 +1,28 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
+        // populate an array of length n with numbers from 1-n
+        // with a nested for loop, compare each of the elements of nums with one element of the array of sequential numbers
+        // if you cannot find one, then it is missing - add it to the new array
+        // this is O(n^2) not good
 
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            int index=Math.abs(nums[i])-1;
-            if(nums[index]<0){
-                continue;
-            }
-            else{
-            nums[index] *=-1;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                list.add(i+1);
+        /**
+        with the same array nums, go through a for loop for 0 through n-1
+        in the array, nums[i] corresponds to a number -- mark nums[number - 1] as negative
+        in the end, then go through the loop again
+        the nums[i] that are positive correspond to the i+1 which is missing */
+
+        for(int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]); // the index we are marking is the current nums[i] - 1
+            if(nums[index-1] > 0) {
+                nums[index-1] *= -1; // negate it and mark it only if not already negative
             }
         }
-        return list;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                ans.add(i+1);
+            }
+        }
+        return ans;
     }
 }
