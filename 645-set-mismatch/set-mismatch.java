39778d87b5b8b639d458aa95bfcf51fb2c64ea85
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-18 22:43:03 +0530

    Time: 15 ms (23.07%) | Memory: 48.5 MB (5.17%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int[] arr=new int[2];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                arr[0]=nums[i];
+            
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            int ind=Math.abs(nums[i])-1;
+            if(nums[ind]>0){
+                nums[ind]*=-1;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                arr[1]=i+1;
+            }
+        }
+
+        return arr;
+    }
+}
