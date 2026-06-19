68f04abd9fb65c7d96c2d5cb5aafa8fffc83e2ba
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-19 06:12:48 +0530

    Time: 6 ms (48.50%) | Memory: 56.2 MB (6.43%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -4,15 +4,18 @@ class Solution {
         int n=nums.length;
         int count=1;
 
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                count++;
-            }
-            if(count>n/2){
-                return nums[i];
-            }
-        }
-        return nums[0];
+        int mid=n/2;
+        return nums[mid];
+
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]==nums[i-1]){
+        //         count++;
+        //     }
+        //     if(count>n/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[0];
     
     }
 }
