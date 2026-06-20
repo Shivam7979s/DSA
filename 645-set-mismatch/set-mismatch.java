762cd7060f438c0f4855bbea47d56f6bbf2c639e
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-20 15:05:23 +0530

    Time: 15 ms (23.09%) | Memory: 48.6 MB (5.17%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        int[] freq = new int[n + 1];
-
-        for (int num : nums) {
-            freq[num]++;
-        }
-
-        int duplicate = -1;
-        int missing = -1;
-
-        for (int i = 1; i <= n; i++) {
-            if (freq[i] == 2) {
-                duplicate = i;
-            }
-            if (freq[i] == 0) {
-                missing = i;
+        Arrays.sort(nums);
+        int[] arr=new int[2];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                arr[0]=nums[i];
+            
             }
         }
+        for(int i=0;i<nums.length;i++){
+            int ind=Math.abs(nums[i])-1;
+            if(nums[ind]>0){
+                nums[ind]*=-1;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                arr[1]=i+1;
+            }
+        }
 
-        return new int[]{duplicate, missing};
+        return arr;
     }
 }
