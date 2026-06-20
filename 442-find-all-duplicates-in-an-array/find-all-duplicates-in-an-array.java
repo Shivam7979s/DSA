2d4903b9a15d81127f25657dca036f890535cab2
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-20 15:03:09 +0530

    Time: 6 ms (89.57%) | Memory: 57.8 MB (61.93%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,21 +1,20 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-       ArrayList<Integer> list = new ArrayList<>();
-
-        int n=nums.length;
-
-        for(int i=0;i<n;i++){
-            int index=Math.abs(nums[i])-1;
-           
-            if(nums[index]>0){
-                nums[index]*=-1;
-            }
-            else{
-                list.add(index+1);
-            }
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            // Get the absolute value to find the mapped 0-based index
+            int index = Math.abs(nums[i]) - 1;
             
+            // If the element at this mapped index is negative, it's a duplicate
+            if (nums[index] < 0) {
+                result.add(index + 1);
+            } else {
+                // Otherwise, flip the sign to mark this number as "seen"
+                nums[index] = -nums[index];
+            }
         }
-        return list;
- 
+        
+        return result;
     }
 }
