4d2436e66416685751a3d87488b15261cca0cf44
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-23 23:17:42 +0530

    Time: 1 ms (99.13%) | Memory: 42.6 MB (51.12%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isUgly(int n) {
         if(n<=0) return false;
-        while(n!=1){
+        while(n>1){
             if(n%2==0){
             n /=2;
             }
