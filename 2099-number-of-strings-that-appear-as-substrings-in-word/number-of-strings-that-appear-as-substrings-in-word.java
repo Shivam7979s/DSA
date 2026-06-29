c42a41f0ead0aaba5cf2874f142d6ea5c22b4cd1
Shivam7979s <shivam.singh97989218@gmail.com>
2026-06-29 07:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (38.69%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             int index=word.indexOf(patterns[i]);
-            if (index != -1) {
+            if (index >=0) {
                 ans++;
             }
         }
