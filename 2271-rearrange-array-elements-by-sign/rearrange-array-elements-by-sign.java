1d01d732d7d428b06cf97733dbc3f1142da7f577
Shivam7979s <shivam.singh97989218@gmail.com>
2026-07-13 00:11:13 +0530

    Time: 3 ms (100.00%) | Memory: 175.7 MB (28.14%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -7,7 +7,7 @@ class Solution {
         int pos=0;
         int neg=1;
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
+            if(nums[i]>=0){
                 ans[pos] = nums[i];
                 pos=pos+2;
             }
