0d2313129364d1a7f8201e7b2ae896681190ada5
Shivam7979s <shivam.singh97989218@gmail.com>
2026-07-15 20:23:44 +0530

    Time: 1 ms (83.11%) | Memory: 42.4 MB (86.19%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,22 +1,23 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
 
-        int oddsum = 1;
-        int odd=0;
+        // int oddsum = 1;
+        // int odd=0;
 
-        int evensum=2;
-        int even=0;
+        // int evensum=2;
+        // int even=0;
 
-        while( n>0){
-            odd=odd+oddsum;
-            oddsum = oddsum+2;
+        // while( n>0){
+        //     odd=odd+oddsum;
+        //     oddsum = oddsum+2;
 
-            even =even+evensum;
-            evensum= evensum+2;
-            n=n-1;
-        }
+        //     even =even+evensum;
+        //     evensum= evensum+2;
+        //     n=n-1;
+        // }
 
-        return Math.abs(odd-even);
-        
+        // return Math.abs(odd-even);
+
+        return n;        
     }
 }
