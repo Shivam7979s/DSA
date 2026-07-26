3aafffdbb32c542920a078a01744e891cb213e6c
Shivam7979s <shivam.singh97989218@gmail.com>
2026-07-27 03:14:50 +0530

    Time: 4 ms (10.08%) | Memory: 43.7 MB (34.23%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] nums) {
+        Arrays.sort(nums);
+        int distinct = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                distinct++;
+            }
+        }
+
+          // Store frequencies
+        int[] freq = new int[distinct];
+
+        int index = 0;
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                freq[index++] = count;
+                count = 1;
+            }
+        }
+
+
+           // Store last frequency
+        freq[freq.length-1] = count;
+        Arrays.sort(freq);
+        
+
+        for(int i=1;i<freq.length;i++){
+            if(freq[i]==freq[i-1]){
+                return false;
+            }
+        }
+
+        
+ 
+        return true;
+    }
+}
