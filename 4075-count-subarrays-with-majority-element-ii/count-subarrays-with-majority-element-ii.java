ca16085703699595f6df165923ef05aad7dcb58c
Shivam7979s <shivam.singh97989218@gmail.com>
2026-08-08 12:07:05 +0530

    Time: 5 ms (55.57%) | Memory: 89.9 MB (29.53%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -5,33 +5,33 @@ class Solution {
 
         for(int i = 0; i < n;i++){
             if(nums[i] == target ){
-                nums[i]=1;
+                nums[i] = 1;
             }
             else{
-                nums[i]=-1;
+                nums[i] = -1;
             }
         }
-        int[] nums2=new int[n];
-        nums2[0]= nums[0];
-        for(int i=1;i<n;i++){
-            nums2[i] = nums2[i-1] + nums[i];
+        int[] nums2 = new int[n];
+        nums2[0] = nums[0];
+        for(int i = 1;i < n;i++){
+            nums2[i] = nums2[i - 1] + nums[i];
 
         }
         int shift = n;
-        int [] freq = new int[2*n+1];
+        int [] freq = new int[2 * n + 1];
 
         freq[shift]= 1;
         long valid = 0;
         int lastsum= 0;
-        for(int i=0;i<n;i++){
-            if(nums2[i]>lastsum ){
-                valid+=freq[lastsum+shift];
+        for(int i = 0 ;i < n;i++){
+            if(nums2[i] > lastsum ){
+                valid += freq[lastsum+shift];
             }
             else{
-                valid-=freq[nums2[i]+shift];
+                valid -= freq[nums2[i] + shift];
             }
-            count +=valid;
-            freq[nums2[i]+shift]++;
+            count += valid;
+            freq[nums2[i] + shift]++;
             lastsum = nums2[i];
 
         }
