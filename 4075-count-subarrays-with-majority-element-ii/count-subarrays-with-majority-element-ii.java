60e4657f6dd9bde6e07dfd99cc405559239d6186
Shivam7979s <shivam.singh97989218@gmail.com>
2026-08-08 12:03:13 +0530

    Time: 5 ms (55.57%) | Memory: 89.6 MB (33.82%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -1,40 +1,40 @@
 class Solution {
     public long countMajoritySubarrays(int[] nums, int target) {
         int n = nums.length;
-        long cnt = 0;
+        long count = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == target) nums[i] = 1;
-            else nums[i] = -1;
-        }
-
-        int[] pref = new int[n];
-        pref[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            pref[i] = pref[i - 1] + nums[i];
-        }
-
-        int shift = n;
-        int[] freq = new int[2 * n + 1];
-
-        freq[shift] = 1;
-
-        long valid = 0;
-        int lastSum = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (pref[i] > lastSum) {
-                valid += freq[lastSum + shift];
-            } else {
-                valid -= freq[pref[i] + shift];
+        for(int i = 0; i < n;i++){
+            if(nums[i] == target ){
+                nums[i]=1;
             }
-
-            cnt += valid;
-            freq[pref[i] + shift]++;
-            lastSum = pref[i];
+            else{
+                nums[i]=-1;
+            }
         }
+        int[] nums2=new int[n];
+        nums2[0]= nums[0];
+        for(int i=1;i<n;i++){
+            nums2[i] = nums2[i-1] + nums[i];
 
-        return cnt;
+        }
+        int shift = n;
+        int [] freq = new int[2*n+1];
+
+        freq[shift]= 1;
+        long valid = 0;
+        int lastsum= 0;
+        for(int i=0;i<n;i++){
+            if(nums2[i]>lastsum ){
+                valid+=freq[lastsum+shift];
+            }
+            else{
+                valid-=freq[nums2[i]+shift];
+            }
+            count +=valid;
+            freq[nums2[i]+shift]++;
+            lastsum = nums2[i];
+
+        }
+        return count ;
     }
 }
