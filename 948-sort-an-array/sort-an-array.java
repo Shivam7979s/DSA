9b7a0a842185362e2b3fd33e03d3fd4d3427391f
Shivam7979s <shivam.singh97989218@gmail.com>
2026-09-09 03:37:27 +0530

    Time: 27 ms (50.16%) | Memory: 74.9 MB (94.56%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,7 +1,44 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+    public int[] sortArray(int[] nums) { 
+        int n = nums.length;
+        if(n<=1){
+            return nums;
+        }
+        int mid = n/2;
+        int[] left = sortArray(Arrays.copyOfRange(nums, 0,mid));
+        int[] right = sortArray(Arrays.copyOfRange(nums, mid,n));
+        return merge(left , right );
+    }
+    public int[] merge(int[] left , int[] right){
+        int r = right.length;
+        int l = left.length;
+        int[] mix = new int[r+l];
+        int i = 0;
+        int j = 0;
+        int k = 0;
 
-        return nums;        
+        while( i<r && j<l){
+            if(right[i]<left[j]){
+                mix[k] = right[i];
+                i++;
+                k++;
+            }
+            else{
+                mix[k] = left[j];
+                j++;
+                k++; 
+            }
+        }
+        while( i < r ){
+            mix[k] = right[i];
+            i++;
+            k++;
+        }
+        while ( j < l){
+            mix[k] = left[j];
+            j++;
+            k++;
+        }
+        return mix;
     }
 }
