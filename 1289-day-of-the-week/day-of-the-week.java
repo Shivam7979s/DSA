960632509a287a0579436ba7cea08b216a88e8c9
Shivam7979s <shivam.singh97989218@gmail.com>
2026-09-10 00:56:02 +0530

    Time: 2 ms (27.27%) | Memory: 42.7 MB (24.44%) - LeetSync

---
+++ 1289-day-of-the-week/day-of-the-week.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+         LocalDate date1 = LocalDate.of(1971, 1, 1);
+          LocalDate date2 = LocalDate.of(year, month, day);
+        long gap = ChronoUnit.DAYS.between(date1, date2);
+        String[] days = { "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday" };
+        int i = (int )gap % 7;
+
+        return days[i];
+    }
+}
