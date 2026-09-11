e6c7cc1cbb87618e56f6e99b1f5920cbd06d71b7
Shivam7979s <shivam.singh97989218@gmail.com>
2026-09-11 06:07:16 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (51.66%) - LeetSync

---
+++ 65-valid-number/valid-number.java
@@ -0,0 +1,74 @@
+class Solution {
+    public boolean isNumber(String s) {
+        boolean digit = false;
+        boolean dot = false;
+        boolean e = false;
+        boolean digitAfterE = true;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            // Digit
+            if (Character.isDigit(ch)) {
+
+                digit = true;
+
+                if (e) {
+                    digitAfterE = true;
+                }
+            }
+
+            // Decimal point
+            else if (ch == '.') {
+
+                // Dot cannot appear after e/E
+                if (e) {
+                    return false;
+                }
+
+                // Only one dot allowed
+                if (dot) {
+                    return false;
+                }
+
+                dot = true;
+            }
+
+            // e or E
+            else if (ch == 'e' || ch == 'E') {
+
+                // Only one e/E allowed
+                if (e) {
+                    return false;
+                }
+
+                // There must be a valid number before e/E
+                if (!digit) {
+                    return false;
+                }
+
+                e = true;
+                digitAfterE = false;
+            }
+
+            // + or -
+            else if (ch == '+' || ch == '-') {
+
+                // Sign is allowed only at beginning
+                // or immediately after e/E
+                if (i != 0 && s.charAt(i - 1) != 'e'
+                        && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            }
+
+            // Anything else
+            else {
+                return false;
+            }
+        }
+
+        return digit && digitAfterE;
+    }
+}
