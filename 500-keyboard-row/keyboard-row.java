86bf2259336f5e937c687a7e8896e7fc1ba2e412
Shivam7979s <shivam.singh97989218@gmail.com>
2026-09-19 18:54:19 +0530

    Time: 2 ms (9.60%) | Memory: 43 MB (32.71%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String first = "qwertyuiop";
+        String second = "asdfghjkl";
+        String third = "zxcvbnm";
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            String[] a = words[i].toLowerCase().split("");
+            boolean flag = true;
+            String temp = "";
+
+            if (first.contains(a[0])) {
+                temp = first;
+            }
+            else if (second.contains(a[0])) {
+                temp = second;
+            }
+            else {
+                temp = third;
+            }
+
+            for (int j = 1; j < a.length; j++)
+                if (!temp.contains(a[j])) {
+                    flag = false;
+                    break;
+                }
+            if (flag)
+                list.add(words[i]);
+        }
+        String[] ans = list.toArray(new String[list.size()]);
+        return ans;
+    }
+}
